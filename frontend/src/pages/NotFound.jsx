18b5cd02642d9c63e086b69619b8ca6191c6a3a7
Purushartha Singh Thakur <purushartha3011@gmail.com>
2025-05-05 02:30:37 +0530

    changed favicon and added img to NotFound page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,7 +1,22 @@
+import NotFoundImg from "@/assets/NotFound.jpg"
+import { Button } from "@/components/ui/button"
+import { useNavigate } from "react-router-dom";
+
+
 
 const NotFound = () => {
+
+  const navigate = useNavigate();
+
   return (
-    <div>NotFound</div>
+    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 w-full">
+      <img src={NotFoundImg} alt="Not Found" className="w-1/4 h-auto" />
+      {/* <h1 className="text-4xl font-bold text-gray-800 mt-4">Page Not Found</h1> */}
+      <p className="text-gray-600 mt-2">Sorry, the page you are looking for does not exist.</p>
+      <Button className={"mt-4"} variant="primary" onClick={() => window.history.length > 2 ? navigate(-1) : navigate('/')}>
+        Go Back
+      </Button>
+    </div>
   )
 }
 
